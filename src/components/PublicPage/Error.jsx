83c3554c74b9053896d227950dfff2cd020612b3
sunyang9876 <sunyang9876@hotmail.com>
2020-09-16 23:04:26 +1000

    add components/publicpage

---
+++ src/components/PublicPage/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = () => (
+  <div className="error__body">
+    <div className="container">
+      <h1>Sorry, the page you are looking for does not exist</h1>
+      <h2>Please check the address and try again.</h2>
+    </div>
+  </div>
+);
+
+export default Error;
